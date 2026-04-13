f29e0f35c816dad5b384d5b5ee384378769bf19e
marco <marcosvmarchiore@gmail.com>
2026-04-13 20:32:15 -0300

    Handle User Input

--- birdle/lib/main.dart
+++ birdle/lib/main.dart
@@ -44,11 +44,7 @@ class Tile extends StatelessWidget {
   }
 }
 
-
-
 class GamePage extends StatelessWidget {
-  GamePage({super.key});
-
   final Game _game = Game();
 
   @override
@@ -62,11 +58,66 @@ class GamePage extends StatelessWidget {
             Row(
               spacing: 5.0,
               children: [
-                for (var letter in guess) Tile(letter.char, letter.type)
-              ]
+                for (var letter in guess) Tile(letter.char, letter.type),
+              ],
             ),
+          GuessInput(
+            onSubmitGuess: (String guess) {
+              // TODO, handle guess
+              print(guess); // Temporary
+            },
+          ),
         ],
       ),
     );
   }
 }
+
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+
+  final void Function(String) onSubmitGuess;
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  final FocusNode _focusNode = FocusNode();
+
+  void _onSubmit() {
+    onSubmitGuess(_textEditingController.text);
+    _textEditingController.clear();
+    _focusNode.requestFocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 5,
+              focusNode: _focusNode,
+              autofocus: true,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(35)),
+                ),
+              ),
+              controller: _textEditingController,
+              onSubmitted: (String value) {
+                _onSubmit();
+              },
+            ),
+          ),
+        ),
+        IconButton(
+          padding: EdgeInsets.zero,
+          icon: const Icon(Icons.arrow_circle_up),
+          onPressed: _onSubmit,
+        ),
+      ],
+    );
+  }
+}
+
