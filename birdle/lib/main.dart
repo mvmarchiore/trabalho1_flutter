5352b0d3dc6792728914cc8950a48d5f721da5dc
marco <marcosvmarchiore@gmail.com>
2026-04-13 20:10:06 -0300

    Correção: Fundamentos de Widget

--- birdle/lib/main.dart
+++ birdle/lib/main.dart
@@ -49,3 +49,22 @@ class Tile extends StatelessWidget {
     );
   }
 }
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+  // This manages game logic, and is out of scope for this lesson.
+  final Game game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          // Add children next.
+        ],
+      ),
+    );
+  }
+}
