0f8842a77c712b1b4c58ee6a90c0bf3db2c115b4
marco <marcosvmarchiore@gmail.com>
2026-04-13 20:33:48 -0300

    Learn about Stateful Widget

--- birdle/lib/main.dart
+++ birdle/lib/main.dart
@@ -44,7 +44,14 @@ class Tile extends StatelessWidget {
   }
 }
 
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
+  GamePage({super.key});
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
   final Game _game = Game();
 
   @override
@@ -52,19 +59,23 @@ class GamePage extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
-        spacing: 5.0,
         children: [
           for (var guess in _game.guesses)
             Row(
-              spacing: 5.0,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                for (var letter in guess) Tile(letter.char, letter.type),
+                for (var letter in guess)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 2.5, vertical: 2.5),
+                    child: Tile(letter.char, letter.type),
+                  )
               ],
             ),
           GuessInput(
-            onSubmitGuess: (String guess) {
-              // TODO, handle guess
-              print(guess); // Temporary
+           onSubmitGuess: (String guess) {
+              setState(() { // NEW
+                _game.guess(guess);
+              });
             },
           ),
         ],
@@ -118,6 +129,20 @@ class GuessInput extends StatelessWidget {
         ),
       ],
     );
+  }
+}
+
+class ExampleWidget extends StatefulWidget {
+  ExampleWidget({super.key});
+
+  @override
+  State<ExampleWidget> createState() => _ExampleWidgetState();
+}
+
+class _ExampleWidgetState extends State<ExampleWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
 
